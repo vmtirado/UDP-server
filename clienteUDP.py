41caf47dfc33fafe9f93676474e2309cba6ea9a9
Vilma Tirado Gomez <vm.tirado@uniandes.edu.co>
2020-04-07 14:15:25 -0500

    First commit

---
+++ clienteUDP.py
@@ -0,0 +1,87 @@
+import socket
+import logging
+import time
+import hashlib
+from _thread import *
+from threading import Thread
+
+def main():
+    # Se inicializa el log
+    logging.basicConfig(filename="clientLog.log", level=logging.INFO,
+                        format='%(asctime)s %(levelname)-8s %(message)s',
+                        datefmt='%Y-%m-%d %H:%M:%S'
+                        )
+
+    #Numero de thread de cliente que se van a crear
+    numt=int(input("Cuantos threads de cliente quiere correr?"))
+    for i in range(numt):
+        #Crea el hash del thread
+        m=hashlib.sha256()
+        try:
+            #Crea el objeto thread
+            thread= Thread(target = threaded, args = (m,i))
+
+            #Se inicia el objeto thred
+            thread.start()
+        except:
+            logging.warning("Error al invocar el thread")
+
+
+def threaded(m,i):
+    start_time=time.time()
+    # local host IP '127.0.0.1'
+    host = '127.0.0.1'
+    #Define the port where you want to connect
+    port=6666
+    s=socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+
+    #Se conecta al servidor
+    s.connect((host, port))
+    print("Thread #%d listo para recibir informacion",i)
+    logging.info("CLIENTE Thread #%d ready to receive info",i)
+    s.sendto("listo".encode(),(host,port))
+    #Se empieza a recibir informacion
+    cont=0
+    num_paquetes=0
+    f = open("ctext.txt", 'wb')
+    while True:
+        data,adrr=s.recvfrom(1024)
+        num_paquetes+=1
+        f.write(data)
+        #print(data)
+        #print(type(data))
+        #print("Cont esta en ",cont)
+        #print(data)
+        if (data.__contains__(b"HASH")):
+            print("Encontre el hash")
+            i=data.find(b"HASH")
+            m.update(data[:i])
+            hash_recibido = data[i+4:]
+            print("El hash recibido es ", hash_recibido)
+            print("El hash calculado es",m.hexdigest())
+            if m.hexdigest()== hash_recibido.decode():
+                print("Hash correcto :)")
+                logging.info("Hash correcto")
+                print("El numero de paquetes recibidos es de ", num_paquetes)
+            else:
+                print("Hash incorrecto :'(")
+                logging.info("Hash incorrecto")
+                print("El numero de paquetes recibidos es de ", num_paquetes)
+            if not data:
+                break
+        m.update(data)
+        cont+=1
+
+    logging.info("CLIENTE Tiempo del envio %s", (time.time() - start_time))
+    logging.info("---------------------------------------------")
+
+    s.close()
+
+
+main()
+
+
+
+
+
+
---
+++ servidorUDP.py
@@ -0,0 +1,81 @@
+import socket
+import time
+from threading import Thread
+import hashlib
+
+
+def main():
+    host='localhost'
+    port=6666
+    s=socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+
+    #Crea el servidor
+    s.bind((host,port))
+
+    print("El servidor esta escuchando en el puerto %d", port)
+    texto=int (input("\n Que texto desea enviar?"
+                     "\n 1. 100 mib"
+                     "\n 2. 250 mib"))
+    if texto==1:
+        f="file1"
+    else:
+        f="file2.txt"
+    global file
+    file=f
+    print("Archivo seleccionado: ",file)
+    num_conn= int( input('\n Cuantas conexiones desea recibir?' ))
+
+    print("Esperando conexiones")
+    cont=0
+    while cont<num_conn:
+
+        #establish connection with client
+        # c is a socket object to send and receive messages
+        #print("cont=",cont)
+        data,adrr=s.recvfrom(1024)
+        print("Recibi conexion",cont)
+        #Crea el thread para manejar la conexion
+        thread=Thread(target= start_thread, args=(s,adrr))
+        thread.start()
+
+def start_thread(s,adrr):
+    start_time=time.time
+    m=hashlib.sha256()
+
+    num_paquetes=0
+    with open(file,'rb') as f:
+        while True:
+            data=f.read(1024)
+            if not data:
+                print ("termine de leer")
+                #envio del hash
+                h=str(m.hexdigest())
+                print("El digest que mando es"+h)
+                s.sendto(("HASH"+h).encode(),adrr)
+                print("El numero de paquetes eviados es de ",num_paquetes)
+                break
+            #print(len(data))
+            #print(data)
+            # start=0
+            # end=1024
+            # while True:
+            #     #print("end esta en ", end)
+            #     sent_data = data[start:end]
+            #     start = end+1
+            #     if end+1025<len(data):
+            #         end+=1025
+            #     else:
+            #         size=len(data)-end
+            #         end=size
+            #     m.update(sent_data)
+            #     #print(sent_data)
+            #     s.sendto(sent_data, adrr)
+            #     if not sent_data:
+            #         print("Termine de partir los fragmentos")
+            #         break
+            m.update(data)
+            s.sendto(data,adrr)
+            num_paquetes+=1
+
+main()
+
